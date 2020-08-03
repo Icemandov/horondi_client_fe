3ee8bf692d8487c1e90d76344a922cae3b608e5c
39312657 <tacjka34@gmail.com>
2020-08-03 20:51:01 +0300

    Responsive filter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -32,7 +32,7 @@ const App = () => {
 
   useEffect(() => {
     dispatch(setThemeMode(localStorageThemeMode));
-  }, [lightMode, dispatch]);
+  }, [lightMode, dispatch, localStorageThemeMode]);
 
   if (isLoading) {
     return (
