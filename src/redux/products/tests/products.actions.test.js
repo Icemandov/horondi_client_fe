091e73101d9b3574a39e76c4ab0c2ab12f685b6a
Dmytro Vasyliev <vas.mytro@gmail.com>
2020-08-20 11:14:40 +0300

    added some tests

--- src/redux/products/tests/products.actions.test.js
+++ src/redux/products/tests/products.actions.test.js
@@ -1,12 +1,26 @@
 import {
   setProductsLoading,
   setProduct,
-  getProduct
+  getProduct,
+  setRate,
+  addComment,
+  updateComment,
+  deleteComment,
+  setComment,
+  setUpdatingComment,
+  setCommentsLoading
 } from '../products.actions';
+
 import {
   SET_PRODUCT,
   SET_PRODUCTS_LOADING,
-  GET_PRODUCT
+  GET_PRODUCT,
+  SET_RATE,
+  SET_COMMENT,
+  ADD_COMMENT,
+  DELETE_COMMENT,
+  UPDATE_COMMENT,
+  SET_COMMENTS_LOADING
 } from '../products.types';
 
 describe('Products actions test', () => {
@@ -47,5 +61,109 @@ describe('Products actions test', () => {
     };
 
     expect(setProductsLoading(true)).toEqual(result);
+  });
+
+  it('should set rate to payload', () => {
+    const newRate = {
+      data: {
+        addRate: {
+          rate: 5
+        }
+      }
+    };
+    const result = {
+      type: SET_RATE,
+      payload: newRate
+    };
+
+    expect(setRate(newRate)).toEqual(result);
+  });
+
+  it('should set comment to payload', () => {
+    const newComment = {
+      data: {
+        addComment: {
+          comment: 'nice'
+        }
+      }
+    };
+
+    const result = {
+      type: SET_COMMENT,
+      payload: newComment
+    };
+
+    expect(setComment(newComment)).toEqual(result);
+  });
+
+  it('should set comment to add to payload', () => {
+    const commentToAdd = {
+      data: {
+        addComment: {
+          comment: 'nice'
+        }
+      }
+    };
+
+    const result = {
+      type: ADD_COMMENT,
+      payload: commentToAdd
+    };
+
+    expect(addComment(commentToAdd)).toEqual(result);
+  });
+
+  it('should set comment to delete to payload', () => {
+    const commentToDelete = {
+      data: {
+        deleteComment: {
+          comment: 'nice'
+        }
+      }
+    };
+
+    const result = {
+      type: DELETE_COMMENT,
+      payload: commentToDelete
+    };
+
+    expect(deleteComment(commentToDelete)).toEqual(result);
+  });
+
+  it('should set comment to delete to payload', () => {
+    const commentToUpdate = {
+      data: {
+        updateComment: {
+          comment: 'nice'
+        }
+      }
+    };
+
+    const result = {
+      type: UPDATE_COMMENT,
+      payload: commentToUpdate
+    };
+
+    expect(updateComment(commentToUpdate)).toEqual(result);
+  });
+
+  it('should set comments loading', () => {
+    const result = {
+      type: SET_COMMENTS_LOADING,
+      payload: true
+    };
+
+    expect(setCommentsLoading(true)).toEqual(result);
+  });
+
+  it('should set updating comment', () => {
+    const commentId = '859238jfj3n4fybu5';
+
+    const result = {
+      type: SET_COMMENTS_LOADING,
+      payload: commentId
+    };
+
+    expect(setCommentsLoading(commentId)).toEqual(result);
   });
 });
--- src/redux/products/tests/products.sagas.test.js
+++ src/redux/products/tests/products.sagas.test.js
@@ -1,10 +1,17 @@
 import { expectSaga } from 'redux-saga-test-plan';
 import { throwError } from 'redux-saga-test-plan/providers';
 import * as matchers from 'redux-saga-test-plan/matchers';
-import getItems from '../../../utils/client';
-import { handleProductLoading } from '../products.sagas';
+
+import { handleProductLoading, handleAddComment } from '../products.sagas';
+import { addComment, getComments, getProduct } from '../products.operations';
+
 import { SET_ERROR } from '../../error/error.types';
-import { SET_PRODUCTS_LOADING, SET_PRODUCT } from '../products.types';
+import {
+  SET_PRODUCTS_LOADING,
+  SET_PRODUCT,
+  SET_COMMENTS_LOADING,
+  SET_COMMENT
+} from '../products.types';
 
 const productId = 'c3a84a5b9866c30390366168';
 
@@ -12,7 +19,7 @@ describe('Products saga', () => {
   it('fetches product', () => {
     const fakeProduct = {
       data: {
-        getProductsById: {
+        getProductById: {
           name: {
             lang: 'en',
             value: 'Rolltop Pink'
@@ -23,20 +30,74 @@ describe('Products saga', () => {
     };
 
     return expectSaga(handleProductLoading, productId)
-      .provide([[matchers.call.fn(getItems), fakeProduct]])
+      .provide([[matchers.call.fn(getProduct), fakeProduct]])
       .put({ type: SET_PRODUCTS_LOADING, payload: true })
-      .put({ type: SET_PRODUCT, payload: fakeProduct.data.getProductsById })
+      .put({ type: SET_PRODUCT, payload: fakeProduct.data.getProductById })
       .put({ type: SET_PRODUCTS_LOADING, payload: false })
       .run();
   });
 
-  it('handles error', () => {
+  it('handles product fetching error', () => {
     const e = new Error('product not found');
 
     return expectSaga(handleProductLoading, productId)
-      .provide([[matchers.call.fn(getItems), throwError(e)]])
+      .provide([[matchers.call.fn(getProduct), throwError(e)]])
       .put({ type: SET_PRODUCTS_LOADING, payload: true })
       .put({ type: SET_PRODUCTS_LOADING, payload: false })
+      .put({ type: SET_ERROR, payload: { e } })
+      .run();
+  });
+});
+
+describe('Add comments saga', () => {
+  it('should add comment', () => {
+    const args = {
+      payload: {
+        rate: 0,
+        product: productId
+      }
+    };
+    const addedComment = {
+      data: {
+        addComment: {
+          text: 'nice'
+        }
+      }
+    };
+    const fakeComments = {
+      data: {
+        getAllCommentsByProduct: {
+          text: 'nice'
+        }
+      }
+    };
+
+    return expectSaga(handleAddComment, args)
+      .provide([
+        [matchers.call.fn(addComment), addedComment],
+        [matchers.call.fn(getComments), fakeComments]
+      ])
+      .put({ type: SET_COMMENTS_LOADING, payload: true })
+      .put({
+        type: SET_COMMENT,
+        payload: fakeComments.data.getAllCommentsByProduct
+      })
+      .put({ type: SET_COMMENTS_LOADING, payload: false })
+      .run();
+  });
+  it('should throw an error', () => {
+    const args = {
+      payload: {
+        rate: 0,
+        product: productId
+      }
+    };
+    const e = new Error('Comment adding fails');
+
+    return expectSaga(handleAddComment, args)
+      .provide([[matchers.call.fn(addComment), throwError(e)]])
+      .put({ type: SET_COMMENTS_LOADING, payload: true })
+      .put({ type: SET_COMMENTS_LOADING, payload: false })
       .put({ type: SET_ERROR, payload: { e } })
       .run();
   });
