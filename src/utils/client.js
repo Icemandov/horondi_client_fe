8a8299a316cd1752acc36eb930a5e843f57a2df1
bandvov <bandvov@gmail.com>
2020-08-24 19:27:38 +0300

    updated

--- src/utils/client.js
+++ src/utils/client.js
@@ -5,7 +5,7 @@ import {
   IntrospectionFragmentMatcher
 } from 'apollo-cache-inmemory';
 
-const introspectionResult = require('../fragmentTypes.json');
+import introspectionResult from 'src/../../fragmentTypes.json';
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData: introspectionResult
